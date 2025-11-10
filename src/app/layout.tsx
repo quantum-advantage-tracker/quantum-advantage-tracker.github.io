82ec1d21c3926a6349c876bb1c40ba2be4120338
Abdón Rodríguez <abdon@ibm.com>
2025-11-10 12:14:49 -0500

    Update header nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { GithubIcon } from '@/icons';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -23,27 +24,33 @@ export default function RootLayout(props: LayoutProps<'/'>) {
     <html lang="en">
       <body className={`${interSans.variable} font-sans antialiased`}>
         <header>
-          <nav className="py-4">
+          <nav className="py-6">
             <div className="mx-auto flex h-full max-w-7xl flex-col items-center justify-between gap-3 px-6 md:flex-row">
               <Link href="/" className="font-semibold">
-                Quantum Advantage Tracker
+                📐 Quantum Advantage Tracker
               </Link>
 
-              <ul className="flex flex-row gap-5">
+              <ul className="flex flex-row gap-4">
                 <li>
-                  <Link href="/trackers">Advantage trackers</Link>
+                  <Button asChild variant="ghost">
+                    <Link href="/trackers">Advantage trackers</Link>
+                  </Button>
                 </li>
                 <li>
-                  <Link href="/participate">Participate</Link>
+                  <Button asChild variant="ghost">
+                    <Link href="/participate">Participate</Link>
+                  </Button>
                 </li>
                 <li>
-                  <a
-                    href="https://github.com/quantum-advantage-tracker/quantum-advantage-tracker.github.io"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <GithubIcon className="size-6" />
-                  </a>
+                  <Button asChild variant="ghost" size="icon">
+                    <a
+                      href="https://github.com/quantum-advantage-tracker/quantum-advantage-tracker.github.io"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <GithubIcon className="size-6" />
+                    </a>
+                  </Button>
                 </li>
               </ul>
             </div>
