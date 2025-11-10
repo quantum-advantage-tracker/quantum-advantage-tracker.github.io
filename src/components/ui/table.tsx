797ad2eca44cd17226d391ba56fbd28ef38b12fc
Abdón Rodríguez <abdon@ibm.com>
2025-11-10 13:26:55 -0500

    Add table header background color

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -69,7 +69,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "text-foreground h-12 px-4 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        "bg-table-head text-table-head-foreground h-12 px-4 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
         className
       )}
       {...props}
